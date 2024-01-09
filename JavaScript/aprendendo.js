3ac35141b64a0619dc8d36729b569bfae22fb06a
Eduardo Alves Ferreira <155549799+eduardoferreiraalest@users.noreply.github.com>
2024-01-09 16:51:20 +0000

    aprendendo

---
+++ JavaScript/aprendendo.js
@@ -0,0 +1,12 @@
+/*
+    COMANDOS PARA FIXAR 
+cd "nome da pasta" ==> entrar em repositório
+cd .. ==> voltar para repositorio anterior 
+
+GIT
+git status ==> fornece um retorno sobre o status
+git add "nome do arquivo"==> adiciona ao git
+git commit - m "teste" ==> para dar um Commit
+git push
+
+*/
